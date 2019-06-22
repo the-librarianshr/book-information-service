4284f03ebec06a11cac2a28890bcde530c6b54ea
Assma Al-Adawi <assma.aladawi@gmail.com>
2019-06-23 00:41:32 +0400

    Implement webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path = require('path');
-const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/public/dist')
+const SRC_DIR = path.join(__dirname, './client/src');
+const DIST_DIR = path.join(__dirname, './public/dist')
 
 module.exports = {
   mode: "development",
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    path:`${DIST_DIR}/index.html`,
-    filename:"bundle.js",
+    path: DIST_DIR,
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015", "react"]
+        }
+      }
+    ]
   }
 };
