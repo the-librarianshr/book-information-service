57ff9ff88b6423b013c122f7f24e1e2925910b6f
Assma Al-Adawi <assma.aladawi@gmail.com>
2019-06-23 00:44:04 +0400

    Add require statements and port num

--- database/index.js
+++ database/index.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
+
+let bookInfoSchema = new Schema ({
+  id: {type: Number, required: true, unique: true},
+  title: {type: String, required: true },
+  author: {type: String, required: true },
+  synopsis: {type: String, required: true },
+  reviews: {type: Number, required: true},
+  average_rating: {type: Number, required: true},
+  number_of_ratings: {type: Number, required: true},
+});
+
+let Book = mongoose.model ('Book', bookInfoSchema);
